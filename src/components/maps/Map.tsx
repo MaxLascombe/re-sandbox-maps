60182e3507e1f1dc4ebfb2bc5db114e421cc8a35
Max Lascombe <mtl2154@columbia.edu>
2026-01-11 22:56:31 -0500

    Moved const outside of component

--- src/components/maps/Map.tsx
+++ src/components/maps/Map.tsx
@@ -12,39 +12,36 @@ interface MapProps {
   address: string;
 }
 
+// Map style configuration
+const mapStyle = {
+  version: 8,
+  sources: {
+    "carto-dark": {
+      type: "raster",
+      tiles: [
+        "https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+        "https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+        "https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
+      ],
+      tileSize: 256,
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    },
+  },
+  layers: [
+    {
+      id: "carto-dark-layer",
+      type: "raster",
+      source: "carto-dark",
+    },
+  ],
+};
+
 const Map: React.FC<MapProps> = ({ latitude, longitude, address }) => {
   const [popupInfo, setPopupInfo] = useState<{
     station: { name: string; lines: string[] };
     lngLat: [number, number];
   } | null>(null);
-
-  // Map style configuration
-  const mapStyle = useMemo(
-    () => ({
-      version: 8,
-      sources: {
-        "carto-dark": {
-          type: "raster",
-          tiles: [
-            "https://a.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
-            "https://b.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
-            "https://c.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png",
-          ],
-          tileSize: 256,
-          attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        },
-      },
-      layers: [
-        {
-          id: "carto-dark-layer",
-          type: "raster",
-          source: "carto-dark",
-        },
-      ],
-    }),
-    []
-  );
 
   // Get nearby subway stations
   const nearbyStations = useMemo(
